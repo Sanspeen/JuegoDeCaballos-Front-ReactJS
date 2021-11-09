1744db6196b15526f4c0f03b2a8701978cd9e5fb
Santiago Franco <ktns0930@gmail.com>
2021-11-09 14:23:50 -0500

    Login sistem is ready and working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from "react/cjs/react.production.min";
+import Home from "./pages/Home";
+import { useState } from "react";
+
 
 function App() {
+
+  const [user, setUser] = useState();
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Home user = {user}></Home>
+      <br></br>
+    </Fragment>
   );
 }
 
