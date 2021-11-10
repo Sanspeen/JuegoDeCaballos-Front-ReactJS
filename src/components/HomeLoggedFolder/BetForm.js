35b0f23c01d367e0b31e41c646c2866ccb530a5c
Santiago Franco <ktns0930@gmail.com>
2021-11-10 17:59:09 -0500

    Start game button now can take the money value.

--- src/components/HomeLoggedFolder/BetForm.js
+++ src/components/HomeLoggedFolder/BetForm.js
@@ -1,14 +1,14 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'; 
 import { Link } from 'react-router-dom';
 
 export default function BetForm(props) {
 
+    const money = useRef();
+
     const handleGame = () =>{
-        
-        props.setUser({"id": props.user.id, "email": props.user.email, "name": props.user.name, "carriel":  0, "money": 1000});
+        props.setUser({"id": props.user.id, "email": props.user.email, "name": props.user.name, "carriel":  0, "money": money.current.value});
     }
-    
 
     return (
         <>
@@ -16,12 +16,12 @@ export default function BetForm(props) {
                 props.isButtonDisabled ? 
                 <>
                         <h6>Ingrese la suma de dinero que apostará.</h6>
-                        <input type = "number"  placeholder = "Dinero" class="form-control"></input>
+                        <input type = "number"  placeholder = "Dinero" class="form-control" ref = {money}></input>
                         <button type = "button" className="btn btn-dark" onClick = {handleGame}>Iniciar juego!</button>
                 </>:<>
                     
                     <h6>Ingrese la suma de dinero que apostará.</h6>
-                    <input placeholder = "Dinero" class="form-control"></input>
+                    <input placeholder = "Dinero" class="form-control" ref = {money}></input>
                     <button className="btn btn-dark" disabled>Iniciar juego!</button>          
                     
                 </>
